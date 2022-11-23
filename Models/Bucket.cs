68fd456f1b2524f828d60469c45c8a7c7a45219f
SENAN111 <behbudovsenan0@gmail.com>
2022-11-23 06:27:16 +0400

    kh

--- Models/Bucket.cs
+++ Models/Bucket.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp9.Models
 {
-    internal class Class1
+    internal class Bucket
     {
+        public string _flower;
+        public int _flowercount;
+        public double _discountprecent;
+        public string Flower
+        {
+            get { return _flower; } set { _flower = value; }
+        }
+        public int Flowercount
+        {
+            get { return _flowercount; }
+            set
+            {
+                if (value > 0 && value <= 100)
+                {
+                    _flowercount = value;
+                }
+            }
+
+        }
+        public  double totalprice()
+        {
+            totalprice_=Flowercount
+        }
     }
 }
--- Models/Flower.cs
+++ Models/Flower.cs
@@ -1,12 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp9.Models
 {
-    internal class Flower
+    internal record Flower         
     {
+        public string name;
+        public string color;
+        public double price;
+        public string Name
+        {
+            get
+            { return name; }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Color
+        {
+            get { return color; }
+            set
+            {
+                if (value.Length > 2 && value.Length < 30)
+                {
+                    color = value;
+                }
+            }
+
+        }
+        public double Price
+        {
+            get { return price; }
+            set 
+            { 
+                if(value>0.1 && value<50000)
+                {
+                    price = value;
+                }
+
+             }
+        }
+        
     }
 }
